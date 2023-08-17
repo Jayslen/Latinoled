a3b814affa2933380279bdafa574bcd8531168aa
Jayslen <jayslen134@gmail.com>
2023-08-17 13:30:21 -0400

    board sketch added

---
+++ src/routes/Home.jsx
@@ -0,0 +1,18 @@
+export function Home() {
+  return (
+    <>
+      <main className="bg-gradient-to-r from-gray-100 to-gray-300 relative">
+        <section className="w-screen h-screen grid place-content-center">
+          <h1 className="text-5xl font-bold">Juega (nombre de la app) </h1>
+          <p className="max-w-sm text-center m-auto">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque,
+            quae eos dicta esse dolores incidunt autem expedita eaque officia
+            alias enim adipisci perferendis dolorem placeat cum corporis nihil
+            rem quasi!
+          </p>
+          <button className="px-4 py-2 bg-red-300 rounded-full">Jugar</button>
+        </section>
+      </main>
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Poppins :'Poppins, sans-serif'
+
+      } 
+    },
   },
   plugins: [],
 }
