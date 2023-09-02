0e70c49e434e2218953c4dfe651e240e03cc9efe
jayslen <jayslen134@gmail.com>
2023-09-02 02:17:07 -0400

    irrelevant changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { GithubIcon, InfoIcon } from './Icons'
 
-export function Header() {
+export function Header () {
   return (
     <header className="text-white flex justify-between p-4 border-b font-Poppins">
       <InfoIcon />
-      <h2 className="text-2xl font-bold">Bobodle</h2>
+      <h2 className="text-2xl font-bold" lang='es' translate='no'>Bobodle</h2>
       <GithubIcon />
     </header>
   )
