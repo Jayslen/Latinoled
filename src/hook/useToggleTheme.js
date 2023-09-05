6c043d9debd8f68fa05606c16b5d69140231d2e0
jayslen <jayslen134@gmail.com>
2023-09-04 20:56:57 -0400

    custom hook to change theme added

--- src/hook/useToggleTheme.js
+++ src/hook/useToggleTheme.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react'
 
 export function useToggleTheme () {
-  const [isDarkModeActive, setIsDarkModeActive] = useState(
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-  )
+  const [isDarkModeActive, setIsDarkModeActive] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches)
 
   const handleCLick = () => {
-    setIsDarkModeActive((prev) => !prev)
+    setIsDarkModeActive(prev => !prev)
   }
+
   useEffect(() => {
     if (isDarkModeActive) {
       document.documentElement.classList.add('dark')
@@ -15,5 +14,5 @@ export function useToggleTheme () {
       document.documentElement.classList.remove('dark')
     }
   }, [isDarkModeActive])
-  return { isDarkModeActive, handleCLick }
+  return { handleCLick, isDarkModeActive }
 }
