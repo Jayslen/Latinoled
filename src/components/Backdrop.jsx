18873f42ad7add5f27c4367c6214351fc6580d7d
jayslen <jayslen134@gmail.com>
2023-09-09 00:06:50 -0400

    backdrop modal component created

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,22 @@
+import { motion } from 'framer-motion'
+
+export function Backdrop ({ children }) {
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="backdrop-blur-2xl absolute top-0 w-screen h-screen grid place-content-center text-[#212529] p-4"
+    >
+      <motion.article
+        initial={{ scale: 0.1, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.5, opacity: 0 }}
+        transition={{ duration: 1, type: 'spring' }}
+        className="bg-[#DEE2E6] w-full h-auto px-4 py-2 flex flex-col gap-2 rounded-sm sm:w-96"
+      >
+        {children}
+      </motion.article>
+    </motion.section>
+  )
+}
