e9b01cd690e5264f60e843656ffcca8ba9fbc2ff
jayslen <jayslen134@gmail.com>
2023-09-13 23:14:58 -0400

    update

--- src/components/RulesModal.jsx
+++ src/components/RulesModal.jsx
@@ -4,13 +4,6 @@ import { IconX } from '@tabler/icons-react'
 import { ruleModalData } from '../constants/MiniBoardData'
 
 export function RulesModal ({ closeModal }) {
-  const row = [
-    { letter: 'M', delayLetter: 0.4, delayDiv: 2.0, color: '#538d4e' },
-    { letter: 'A', delayLetter: 0.6, delayDiv: 2.2, color: '#b59f3b' },
-    { letter: 'N', delayLetter: 0.8, delayDiv: 2.4, color: '#b59f3b' },
-    { letter: 'G', delayLetter: 1, delayDiv: 2.6, color: '#3a3a3c' },
-    { letter: 'U', delayLetter: 1.2, delayDiv: 2.8, color: '#3a3a3c' }
-  ]
   return (
         <Backdrop>
           <div className='absolute -top-14 right-0 bg-[#DEE2E6] hover:bg-red-500 hover:scale-95 rounded-full p-2 group transition-colors cursor-pointer' onClick={closeModal}>
