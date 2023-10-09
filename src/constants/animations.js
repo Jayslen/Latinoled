edd95992cbe22f47d0c909097f5f88c652b745f6
jayslen <jayslen134@gmail.com>
2023-10-09 12:52:27 -0400

    framer motion animations move to a const ('some')

--- src/constants/animations.js
+++ src/constants/animations.js
@@ -2,3 +2,8 @@ export const headerIconsAnimation = {
   tap: { scale: 0.8 },
   hover: { scale: 1.12 }
 }
+export const headerModalsAnimation = {
+  hidden: { opacity: 0, translateY: '-80px' },
+  visible: { opacity: 1, translateY: '0px' },
+  exit: { opacity: 0 }
+}
